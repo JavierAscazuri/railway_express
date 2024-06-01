1fd6ec6232b0b6c05891064702cf8741f31ef801
Javier <javierascazuri@gmail.com>
2024-06-01 12:02:29 -0300

    Actualizacion

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const express=require("express");
-const bodyParse=require("body-parser");
+const venom = require('venom-bot');
 
-const app=express();
-const PORT= process.env.PORT || 3977;
+venom
+  .create({
+    session: 'session-name' //name of session
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
 
-app.use(bodyParse.urlencoded({extended:true}));
-app.use(bodyParse.json());
+function start(client) {
 
-app.get("/", (req,res)=>{
-    res.status(200).send({msg:"Hola Javier Ascazuri"});
-});
+    const express=require("express");
+    const bodyParse=require("body-parser");
+    
+    const app=express();
+    const PORT= process.env.PORT || 3977;
+    
+    app.use(bodyParse.urlencoded({extended:true}));
+    app.use(bodyParse.json());
 
-app.post("/welcome", (req,res)=>{
-    const {username}=rep.body;
-    res.status(200).send({msg:"Hola: "+username});
-});
+    app.get("/", (req,res)=>{
+        res.status(200).send({msg:"Hola Javier Ascazuri"});
+        client.sendText('5492494480219@c.us', 'Se ha cargado un nuevo Expediente')
+    });
+    app.post("/welcome", (req,res)=>{
+        const {username}=rep.body;
+        res.status(200).send({msg:"Hola: "+username});
+    });
 
-app.listen(PORT, () =>{
-    console.log("tu servidor esta listo en el puerto "+PORT)
-})
+    app.listen(PORT, () =>{
+        console.log("tu servidor esta listo en el puerto "+PORT)
+    })
+
+}
+
+
+
+
